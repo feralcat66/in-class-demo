dbc547ace27746b5e590b1f013b4489d2f405f19
Rex Bergeron <rexbergeron@comcast.net>
2020-02-21 09:36:57 -0800

    create table

--- data/create-tables.js
+++ data/create-tables.js
@@ -1,21 +1,42 @@
+// load connection string from .env
 require('dotenv').config();
+// "require" pg (after `npm i pg`)
 const pg = require('pg');
+// Use the pg Client
 const Client = pg.Client;
+// **note:** you will need to create the database!
 
+// async/await needs to run in a function
 run();
 
 async function run() {
+    // make a new pg client to the supplied url
     const client = new Client(process.env.DATABASE_URL);
 
     try {
+        // initiate connecting to db
         await client.connect();
+    
+        // run a query to create tables
         await client.query(`
+            CREATE TABLE instruments (
+                instrument SERIAL PRIMARY KEY NOT NULL,
+                main_strings VARCHAR(256) NOT NULL,
+                bowed BOOLEAN NOT NULL,
+                origin VARCHAR(256) NOT NULL,
+                url INTEGER NOT NULL
+            );
         `);
+
+        console.log('create tables complete');
     }
     catch (err) {
+        // problem? let's see the error...
         console.log(err);
     }
     finally {
+        // success or failure, need to close the db connection
         client.end();
     }
+    
 }
